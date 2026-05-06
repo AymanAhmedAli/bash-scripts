8497a4d4fe8549ed970e2adf8732a4825c5d8b03
AymanAhmedAli <aymanahmedofficiaal@gmail.com>
2026-05-06 22:51:14 +0300

    clean up files

---
+++ check.sh
@@ -0,0 +1,13 @@
+if [ -d"$HOME/Doucments" ]; then
+   echo "Documents : Exists"
+else
+   echo "Doucments : Dosn't Exists"
+fi
+
+if [ -d"$HOME/ayman/linux_practice" ]; then
+   echo "Linux_practice : Exists"
+else
+   echo "linux_practice : Dosen't  Exists "
+fi
+
+
